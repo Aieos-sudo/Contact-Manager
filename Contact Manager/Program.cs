cdc9584fc7bd6c48b2f4cd1c03452d6c5b195403
Aieos <firered9000@yahoo.com>
2022-01-21 21:17:03 -0600

    Added comments

--- Contact Manager/Program.cs
+++ Contact Manager/Program.cs
@@ -5,6 +5,10 @@ namespace Contact_Manager
 {
     internal class Program
     {
+        /// <summary>
+        /// Allows entry, storage, and removal of contact information
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             List<List<string>> Contacts = new List<List<string>>();
@@ -20,10 +24,16 @@ namespace Contact_Manager
             }
             while (enteredcommand != "exit");
         }
+        /// <summary>
+        /// Displays title for the program
+        /// </summary>
         static void Title()
         {
             Console.WriteLine("Contact Manager");
         }
+        /// <summary>
+        /// Lists available commands
+        /// </summary>
         static void CommandMenu()
         {
             Console.WriteLine("COMMAND MENU");
@@ -33,12 +43,20 @@ namespace Contact_Manager
             Console.WriteLine("del - Delete a contact");
             Console.WriteLine("exit - Exit program\n");
         }
+        /// <summary>
+        /// Allows user to enter a command from the CommandMenu()
+        /// </summary>
+        /// <returns></returns>
         static string CommandEntry()
         {
             Console.Write("Command: ");
             string enteredcommand = Console.ReadLine();
             return enteredcommand;
         }
+        /// <summary>
+        /// Lists all available contacts
+        /// </summary>
+        /// <param name="Contacts">Holds list of contacts</param>
         static void CommandList(List<List<string>> Contacts)
         {
             if (Contacts.Count != 0)
@@ -54,6 +72,11 @@ namespace Contact_Manager
                 Console.WriteLine("No contacts available.\n");
             }
         }
+        /// <summary>
+        /// Views data stored for a specified user
+        /// </summary>
+        /// <param name="Contacts">Holds list of contacts</param>
+        /// <param name="Info">Gives info about each contacts stored data</param>
         static void CommandView(List<List<string>> Contacts, List<string> Info)
         {
             if (Contacts.Count != 0)
@@ -71,6 +94,11 @@ namespace Contact_Manager
                 Console.WriteLine("No contacts available.\n");
             }
         }
+        /// <summary>
+        /// Adds a new user
+        /// </summary>
+        /// <param name="Contacts">Holds a list of contacts</param>
+        /// <param name="Info">Gives info about each contacts stored data</param>
         static void CommandAdd(List<List<string>> Contacts, List<string> Info)
         {
             List<string> ContactInfo = new List<string> { };
@@ -82,6 +110,10 @@ namespace Contact_Manager
             Contacts.Add(ContactInfo);
             Console.WriteLine(ContactInfo[0] + " was added.\n");
         }
+        /// <summary>
+        /// Lets user delete specific contacts
+        /// </summary>
+        /// <param name="Contacts"></param>
         static void CommandDel(List<List<string>> Contacts)
         {
             if (Contacts.Count != 0)
@@ -96,6 +128,12 @@ namespace Contact_Manager
                 Console.WriteLine("No contacts available.\n");
             }
         }
+        /// <summary>
+        /// Determines which command to run, and runs it. As well as handles exceptions.
+        /// </summary>
+        /// <param name="Contacts">Holds a list of contacts</param>
+        /// <param name="Info">Gives info about each contacts stored data</param>
+        /// <param name="enteredcommand">Stores the command entered from CommandEntry()</param>
         static void CaseList(List<List<string>> Contacts, List<string> Info, string enteredcommand)
         {
             try
@@ -143,6 +181,6 @@ namespace Contact_Manager
             {
                 Console.WriteLine("An error has occured");
             }
-        }
+        } 
     }
 }
